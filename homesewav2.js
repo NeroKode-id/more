25ecc64463394a69bca43e58ff82129b304e082e
NeroKode-id <nerokode@gmail.com>
2025-05-17 09:15:50 +0700

    Create homesewav2.js

---
+++ homesewav2.js
@@ -0,0 +1,80 @@
+// Jangan edit apapun !!!!!
+const Server = "https://nerokode.com";
+function Pulsa() {
+    window.location.href = Server + '/Bukaolshop_Script/Pulsa' + versi + '?nama_apk=' + namaAplikasi + '&kategori=Pulsa';
+}
+
+function TVprabayar(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/TVprabayar' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function TokenPLN(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/TokenPLN' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function BebasNominal(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/BebasNominal' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function Ewallet(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/Ewallet' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function Game(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/Game' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function GameGi(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/GameGi' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function GameMl(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/GameMl' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function GameLa(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/GameLa' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function MasaAktif(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/MasaAktif' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function PaketSMS(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/PaketSMS' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function PaketTelpon(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/PaketTelepon' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function InjectSatuan(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/VoucherInject' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function InjectMasal(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/InjectMasal' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function PaketData(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/PaketData' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
+
+function Tagihan(element) {
+    const kategori = element.querySelector('p').innerText;
+    window.location.href = Server + '/Bukaolshop_Script/Tagihan' + versi + '?nama_apk=' + namaAplikasi + '&kategori=' + encodeURIComponent(kategori);
+}
